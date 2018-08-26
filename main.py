a3b614090d9cdcc376a36c81e205048046e0c38f
Kohei YOSHIDA <kohei@yosida95.com>
2018-08-26 15:42:55 +0900

    rename WSGI entrypoint to main:app to meet the GAE default

--- main.py
+++ main.py
@@ -4,12 +4,11 @@ import time
 from datetime import datetime
 
 
-def application(environ, start_response):
-    headers = [
+def app(environ, start_response):
+    start_response('200 OK', [
         ('Content-Type', 'text/plain; charset=utf8'),
-    ]
-    start_response('200 OK', headers)
+    ])
 
     now = datetime.now()
     unixtime = int(time.mktime(now.timetuple()))
-    return str(unixtime).encode('utf8')
+    return [str(unixtime).encode('utf8')]
