06169c38e5fa3c9c0d49cb8103df5488c0132bfd
Kohei YOSHIDA <kohei@yosida95.com>
2018-09-04 17:46:14 +0900

    check HTTP request method

--- main.py
+++ main.py
@@ -4,6 +4,17 @@ from datetime import datetime
 
 
 def app(environ, start_response):
+    request_method = environ.get('REQUEST_METHOD')
+    if request_method not in ('GET', 'HEAD'):
+        start_response('405 Method Not Allowed', [
+            ('Cache-Control', 'no-store'),
+            ('Content-Security-Policy', "default-src: none"),
+            ('Methods', 'GET,HEAD'),
+            ('X-Content-Type-Options', 'nosniff'),
+            ('X-XSS-Protection', '1; mode=block'),
+        ])
+        return []
+
     start_response('200 OK', [
         ('Cache-Control', 'no-store'),
         ('Content-Type', 'text/plain; charset=utf8'),
@@ -11,6 +22,8 @@ def app(environ, start_response):
         ('X-Content-Type-Options', 'nosniff'),
         ('X-XSS-Protection', '1; mode=block'),
     ])
+    if request_method == 'HEAD':
+        return []
 
     unixtime = int(datetime.now().timestamp())
     return [str(unixtime).encode('utf8')]
